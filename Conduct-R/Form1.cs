303f40aef2c41de67353d1798632e6677c6fd37b
Matthew Kaes <matthew.kaes@digipen.edu>
2016-03-08 23:28:05 -0800

    Booting Up R Engine
    Setting up the R engine in Conductor.

--- Conduct-R/Form1.cs
+++ Conduct-R/Form1.cs
@@ -8,16 +8,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RChestration.Utilities;
+using RDotNet;
 
 namespace Conduct_R
 {
   public partial class Form1 : Form
   {
     Dictionary<string, List<string>> rDataFrame = null;
+    REngine rEngine = null;
 
     public Form1()
     {
       InitializeComponent();
+
+      // Setup the R-Engine to use R
+      rEngine = REngine.GetInstance();
+      rEngine.Initialize();
+
+      // Install all of our required packages.
+      rEngine.Evaluate("list.of.packages <- c(\"ggplot2\", \"Cairo\")");
+      rEngine.Evaluate("new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,\"Package\"])]");
+      rEngine.Evaluate("if(length(new.packages)) install.packages(new.packages)");
     }
 
     private void Form1_Load(object sender, EventArgs e)
@@ -55,12 +66,19 @@ namespace Conduct_R
         dataFrameHeader1.SelectedIndex = 0;
         dataFrameHeader1.Enabled = true;
         plotButton.Enabled = true;
+
+        DataTranslator.RImportDataFrame(rEngine, rDataFrame);
       }
       else
       {
         dataFrameHeader1.Enabled = false;
         plotButton.Enabled = false;
       }
+    }
+
+    private void RenderData()
+    {
+
     }
   }
 }
---
+++ R-chestration/DataTranslator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDotNet;
+using System.Collections;
+
+namespace RChestration.Utilities
+{
+  public class DataTranslator
+  {
+    public static void RImportDataFrame(REngine targetEngine, Dictionary<string, List<string>> rDataFrame)
+    {
+      SortedSet<string> headerSet = new SortedSet<string>(rDataFrame.Keys);
+      SortedDictionary<string, List<string>> sortedFrame = new SortedDictionary<string, List<string>>(rDataFrame);
+
+      IEnumerable[] dataBlock = new IEnumerable[rDataFrame.Keys.Count()];
+      for(int index = 0; index < rDataFrame.Keys.Count(); index++)
+      {
+        dataBlock[index] = sortedFrame.Select(e => e.Value[index]).ToArray();
+      }
+
+      DataFrame df = targetEngine.CreateDataFrame(dataBlock, headerSet.ToArray());
+      targetEngine.SetSymbol("impData", df);
+    }
+  }
+}
